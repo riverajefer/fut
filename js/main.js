ccd9d8bf08cc92890c524101676f47e0ca5b3e62
unknown <=riverajefer@gmail.com>
2018-02-10 14:46:01 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,81 @@
+$('#carousel-example').carousel();
+var winWidth = $(window).innerWidth();
+$(window).resize(function() {
+
+    if ($(window).innerWidth() < winWidth) {
+        $('.carousel-inner>.item>img').css({
+            'min-width': winWidth,
+            'width': winWidth
+        });
+    } else {
+        winWidth = $(window).innerWidth();
+        $('.carousel-inner>.item>img').css({
+            'min-width': '',
+            'width': ''
+        });
+    }
+});
+
+
+$(document).ready(function() {
+    $('.slide_clientes').slick({
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 8,
+        responsive: [{
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+});
+
+
+
+$(window).scroll(function(event) {
+    var scroll = $(window).scrollTop();
+    /*     if (scroll > 200) {
+            $('.top-nav-collapse').fadeOut();
+        } else {
+            $('.top-nav-collapse').fadeIn();
+        } */
+});
+
+
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtnGoTop").style.display = "block";
+    } else {
+        document.getElementById("myBtnGoTop").style.display = "none";
+    }
+}
+
+
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+    $('.navbar-toggle').hide();
+
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+    $('.navbar-toggle').show();
+}
