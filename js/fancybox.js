09df236deb283f98eabbbd72102a39fee7e9d573
Yderfre <61797507+Yderfre@users.noreply.github.com>
2020-06-14 11:25:44 -0500

    Se agrego la galeria de imagenes con fancybox, se cambio la imformacion en el footer se cambio el fondo el section servicios

---
+++ js/fancybox.js
@@ -0,0 +1,150 @@
+// jQuery_1_3_2('#selector').function(); o jQuery_1_1_3('#selector').function();.
+
+// jQuery_1_4_3('a[rel=optica]').fancybox({
+
+// $(document).ready(function () {
+//   $oldjQuery('a[rel=optica]').fancybox({
+//     transitionIn: 'fade',
+//     overlayColor: '#000',
+//     overlayOpacity: 0.9,
+//     // titlePosition: 'over',
+//     titlePosition: 'outside',
+//     titleFormat: function (title, currentArray, currentIndex, currentOpts) {
+//       return (
+//         '<span id="fancybox-title-over">Proyectos ' +
+//         (currentIndex + 1) +
+//         ' de ' +
+//         currentArray.length +
+//         (title.length ? ' &nbsp; ' + title : '') +
+//         '</span>'
+//       );
+//     },
+//   });
+// });
+
+// $(document).ready(function () {
+//   $oldjQuery('a[rel=kokoriko]').fancybox({
+//     overlayColor: '#000',
+//     overlayOpacity: 0.9,
+//     titlePosition: 'outside',
+//     titleFormat: function (title, currentArray, currentIndex, currentOpts) {
+//       return (
+//         '<span id="fancybox-title-over">Proyectos ' +
+//         (currentIndex + 1) +
+//         ' de ' +
+//         currentArray.length +
+//         (title.length ? ' &nbsp; ' + title : '') +
+//         '</span>'
+//       );
+//     },
+//   });
+// });
+
+// $(document).ready(function () {
+//   $oldjQuery('a[rel=limpieza]').fancybox({
+//     overlayColor: '#000',
+//     overlayOpacity: 0.9,
+//     titlePosition: 'outside',
+//     titleFormat: function (title, currentArray, currentIndex, currentOpts) {
+//       return (
+//         '<span id="fancybox-title-over">Proyectos ' +
+//         (currentIndex + 1) +
+//         ' de ' +
+//         currentArray.length +
+//         (title.length ? ' &nbsp; ' + title : '') +
+//         '</span>'
+//       );
+//     },
+//   });
+// });
+
+// $(document).ready(function () {
+//   $oldjQuery('a[rel=experiencia]').fancybox({
+//     overlayColor: '#000',
+//     overlayOpacity: 0.9,
+//     titlePosition: 'outside',
+//     titleFormat: function (title, currentArray, currentIndex, currentOpts) {
+//       return (
+//         '<span id="fancybox-title-over">Proyectos ' +
+//         (currentIndex + 1) +
+//         ' de ' +
+//         currentArray.length +
+//         (title.length ? ' &nbsp; ' + title : '') +
+//         '</span>'
+//       );
+//     },
+//   });
+// });
+
+$k(document).ready(function () {
+  $k('a[rel=optica]').fancybox({
+    transitionIn: 'fade',
+    overlayColor: '#000',
+    overlayOpacity: 0.9,
+    // titlePosition: 'over',
+    titlePosition: 'outside',
+    titleFormat: function (title, currentArray, currentIndex, currentOpts) {
+      return (
+        '<span id="fancybox-title-over">Proyectos ' +
+        (currentIndex + 1) +
+        ' de ' +
+        currentArray.length +
+        (title.length ? ' &nbsp; ' + title : '') +
+        '</span>'
+      );
+    },
+  });
+});
+
+$k(document).ready(function () {
+  $k('a[rel=kokoriko]').fancybox({
+    overlayColor: '#000',
+    overlayOpacity: 0.9,
+    titlePosition: 'outside',
+    titleFormat: function (title, currentArray, currentIndex, currentOpts) {
+      return (
+        '<span id="fancybox-title-over">Proyectos ' +
+        (currentIndex + 1) +
+        ' de ' +
+        currentArray.length +
+        (title.length ? ' &nbsp; ' + title : '') +
+        '</span>'
+      );
+    },
+  });
+});
+
+$k(document).ready(function () {
+  $k('a[rel=limpieza]').fancybox({
+    overlayColor: '#000',
+    overlayOpacity: 0.9,
+    titlePosition: 'outside',
+    titleFormat: function (title, currentArray, currentIndex, currentOpts) {
+      return (
+        '<span id="fancybox-title-over">Proyectos ' +
+        (currentIndex + 1) +
+        ' de ' +
+        currentArray.length +
+        (title.length ? ' &nbsp; ' + title : '') +
+        '</span>'
+      );
+    },
+  });
+});
+$k(document).ready(function () {
+  $k('a[rel=experiencia]').fancybox({
+    overlayColor: '#000',
+    overlayOpacity: 0.9,
+    titlePosition: 'outside',
+    titleFormat: function (title, currentArray, currentIndex, currentOpts) {
+      return (
+        '<span id="fancybox-title-over">Proyectos ' +
+        (currentIndex + 1) +
+        ' de ' +
+        currentArray.length +
+        (title.length ? ' &nbsp; ' + title : '') +
+        '</span>'
+      );
+    },
+  });
+});
--- js/main.js
+++ js/main.js
@@ -1,81 +1,112 @@
 $('#carousel-example').carousel();
 var winWidth = $(window).innerWidth();
-$(window).resize(function() {
-
-    if ($(window).innerWidth() < winWidth) {
-        $('.carousel-inner>.item>img').css({
-            'min-width': winWidth,
-            'width': winWidth
-        });
-    } else {
-        winWidth = $(window).innerWidth();
-        $('.carousel-inner>.item>img').css({
-            'min-width': '',
-            'width': ''
-        });
-    }
-});
-
-
-$(document).ready(function() {
-    $('.slide_clientes').slick({
-        centerMode: true,
-        centerPadding: '60px',
-        slidesToShow: 8,
-        responsive: [{
-                breakpoint: 768,
-                settings: {
-                    arrows: false,
-                    centerMode: true,
-                    centerPadding: '40px',
-                    slidesToShow: 3
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    arrows: false,
-                    centerMode: true,
-                    centerPadding: '40px',
-                    slidesToShow: 1
-                }
-            }
-        ]
+$(window).resize(function () {
+  if ($(window).innerWidth() < winWidth) {
+    $('.carousel-inner>.item>img').css({
+      'min-width': winWidth,
+      width: winWidth,
     });
+  } else {
+    winWidth = $(window).innerWidth();
+    $('.carousel-inner>.item>img').css({
+      'min-width': '',
+      width: '',
+    });
+  }
 });
 
+// $(document).ready(function () {
+//   $('.slide_clientes').slick({
+//     centerMode: true,
+//     centerPadding: '60px',
+//     slidesToShow: 8,
+//     responsive: [
+//       {
+//         breakpoint: 768,
+//         settings: {
+//           arrows: false,
+//           centerMode: true,
+//           centerPadding: '40px',
+//           slidesToShow: 3,
+//         },
+//       },
+//       {
+//         breakpoint: 480,
+//         settings: {
+//           arrows: false,
+//           centerMode: true,
+//           centerPadding: '40px',
+//           slidesToShow: 1,
+//         },
+//       },
+//     ],
+//   });
+// });
 
-
-$(window).scroll(function(event) {
-    var scroll = $(window).scrollTop();
-    /*     if (scroll > 200) {
+$(window).scroll(function (event) {
+  var scroll = $(window).scrollTop();
+  /*     if (scroll > 200) {
             $('.top-nav-collapse').fadeOut();
         } else {
             $('.top-nav-collapse').fadeIn();
         } */
 });
 
-
-
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() { scrollFunction() };
+window.onscroll = function () {
+  scrollFunction();
+};
 
 function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("myBtnGoTop").style.display = "block";
-    } else {
-        document.getElementById("myBtnGoTop").style.display = "none";
-    }
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById('myBtnGoTop').style.display = 'block';
+  } else {
+    document.getElementById('myBtnGoTop').style.display = 'none';
+  }
 }
 
-
 function openNav() {
-    document.getElementById("myNav").style.width = "100%";
-    $('.navbar-toggle').hide();
-
+  document.getElementById('myNav').style.width = '100%';
+  $('.navbar-toggle').hide();
 }
 
 function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
-    $('.navbar-toggle').show();
+  document.getElementById('myNav').style.width = '0%';
+  $('.navbar-toggle').show();
 }
+
+$(document).ready(function () {
+  $('.slide_clientes').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});
+
+// fancybox-galeria-proyectos
